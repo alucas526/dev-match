303d61efb93bc237abfb1e2d10843ce27f1f5db2
Alan Lucas <alucas@gmail.com>
2016-08-26 17:30:25 +0000

    Added about page view, action, and route.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # root 'welcome#index'
 
 root 'pages#home'
+get '/about' => 'pages#about'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
